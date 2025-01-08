21259dad7b9ad90e85eed3b33f21e7cc838d5296
LuaXdea <144655350+Contra-Sakar@users.noreply.github.com>
2025-01-08 19:03:10 -0400

    Nueva función 'setCameraStrum'

--- psychlua/ExtraFunctionsLua.lua
+++ psychlua/ExtraFunctionsLua.lua
@@ -37,5 +37,40 @@ function onCreate()
         parentLua.call('setProperty',[tag + '.x',ValorX]);
         parentLua.call('setProperty',[tag + '.y',ValorY]);
     });
+    createGlobalCallback('setCameraStrum',function(strumGroup:String,camera:String,?unspawnNotes:Bool = true,?scrollFactor:Dynamic = null) {
+    var targetGroup = Reflect.field(game,strumGroup);
+    var cameraObj = Reflect.field(game,camera);
+    if (targetGroup == null || cameraObj == null) {
+        debugPrint('Error: Specified group or camera does not exist - ' + strumGroup + ', ' + camera + '\nError: El grupo o la cámara especificados no existen - ' + strumGroup + ', ' + camera, FlxColor.RED);
+        return;
+    }
+    for (strumOp in targetGroup) {
+        strumOp.cameras = [cameraObj];
+        if (scrollFactor != null && scrollFactor.length == 2) {
+            strumOp.scrollFactor.set(scrollFactor[0], scrollFactor[1]);
+        }
+    }
+    if (unspawnNotes == null || unspawnNotes) {
+        for (note in game.unspawnNotes) {
+            if ((strumGroup == 'playerStrums' && note.mustPress) || 
+                (strumGroup == 'opponentStrums' && !note.mustPress) || 
+                strumGroup == 'strumLineNotes') {
+                note.cameras = [cameraObj];
+                if (scrollFactor != null && scrollFactor.length == 2) {
+                    note.scrollFactor.set(scrollFactor[0], scrollFactor[1]);
+                }
+            }
+        }
+    }
+    var noteSplashes = game.grpNoteSplashes;
+    if (noteSplashes != null) {
+        for (splash in noteSplashes) {
+            splash.cameras = [cameraObj];
+            if (scrollFactor != null && scrollFactor.length == 2) {
+                splash.scrollFactor.set(scrollFactor[0], scrollFactor[1]);
+                }
+            }
+        }
+    });
     ]])
 end
